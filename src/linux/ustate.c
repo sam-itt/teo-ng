5e6fbdf7303a349ca93f6509597a497373d46431
Fran?ois Mouret <fjjm@orange.fr>
2012-02-29 18:19:05 +0100

    Update of ustate.c

--- src/linux/ustate.c
+++ src/linux/ustate.c
@@ -14,7 +14,7 @@
  *
  *                  L'émulateur Thomson TO8
  *
- *  Copyright (C) 1997-2011 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
+ *  Copyright (C) 1997-2012 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
  *                          Jérémie Guillaume, François Mouret
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -34,30 +34,26 @@
 
 /*
  *  Module     : linux/ustate.c
- *  Version    : 1.8.0
+ *  Version    : 1.8.1
  *  Créé par   : François Mouret 27/01/2010
- *  Modifié par: François Mouret 14/09/2011
+ *  Modifié par: François Mouret 14/09/2011 17/01/2012
  *
- *  Module de chargement/sauvegarde de l'état de l'émulateur.
+ *  Chargement/Sauvegarde de l'état de l'émulateur.
  */
 
 #ifndef SCAN_DEPEND
    #include <stdio.h>
    #include <stdlib.h>
    #include <string.h>
+   #include <ctype.h>
    #include <sys/stat.h>
    #include <unistd.h>
+   #include <gtk/gtk.h>
 #endif
 
 #include "intern/defs.h"
 #include "linux/main.h"
 #include "to8.h"
-
-enum {
-    LIST_READ,
-    LIST_WRITE,
-    LIST_DELETE
-};
 
 struct CFG_LIST {
     char *line;
@@ -65,6 +61,8 @@ struct CFG_LIST {
 };
 
 static struct CFG_LIST *list_start = NULL;
+static int load_error = 0;
+
 
 
 /* UnloadIni:
@@ -72,169 +70,149 @@ static struct CFG_LIST *list_start = NULL;
  */
 static void UnloadCfg(void)
 {
-    struct CFG_LIST *list = list_start;
     struct CFG_LIST *next = NULL;
 
-    while(list != NULL)
-    {
-        next = list->next;
-        if (list->line != NULL)
-            free(list->line);
-        free(list);
-        list = next;
+    while(list_start != NULL) {
+        next = list_start->next;
+        if (list_start->line != NULL)
+            free(list_start->line);
+        free(list_start);
+        list_start = next;
     }
 }
 
 
 
-/* InsertConfigLine:
+/* CreateConfigLine:
  * Insère une ligne dans la liste
  */
-static struct CFG_LIST *InsertConfigLine(struct CFG_LIST *list_prev, char *line)
+static struct CFG_LIST *CreateConfigLine (char *line)
 {
     struct CFG_LIST *list = NULL;
 
-    if ((list = (struct CFG_LIST *)malloc (sizeof (struct CFG_LIST))) == NULL)
-        return NULL;
-    list->next = NULL;
-
-    if ((list->line = (char *)malloc (strlen(line)+1)) == NULL) {
-        free (list);
-        return NULL; }
-    strcpy (list->line, line);
-
-    if (list_prev == NULL)
-        list_start = list;
-    else {
-        list->next = list_prev->next;
-        list_prev->next = list; }
-
-    return list;
-}
-
-
-
-/* SkipSpaces:
- *  Passe les espaces préalables d'une string.
- */
-static char *SkipSpaces(char *ptr)
-{
-    while ((*ptr <= 0x20) && (*ptr != '\0'))
-        ptr++;
-    return ptr;
-}
-    
-
-
-/* FindCongigList:
- *  Pointe sur la CFG_LIST selon la section et la clef.
- */
-static struct CFG_LIST *FindConfigList(char *section, char *key, int mode)
-{
-    struct CFG_LIST *list = list_start;
-    struct CFG_LIST *list_last = NULL;
-    struct CFG_LIST *list_prev = NULL;
-    char line[50] = "";
-
-    /* Recherche la section */
-    while ((list != NULL)
-        && ((list->line[0] != '[')
-         || (strncmp(section, list->line+1, strlen(section)) != 0)
-         || (list->line[strlen(section)+1] != ']')))
-        list = list->next;
-
-    if (list == NULL)
-        return NULL;
-
-    /* Recherche la clef */
-    while (strncmp(key, list->line, strlen(key)) != 0)
-    {
-        list_prev = list;
-        if (list->line[0] != '\0') list_last = list;
-        list = list->next;
-
-        if ((list == NULL)
-         || (list->line[0] == '['))
-        {
-            if (mode == LIST_WRITE) {
-                (void)snprintf(line, 50, "%s = ", key);
-                list_last->next = InsertConfigLine(list_last, line);
-                return list_last->next; }
-
-            return NULL;
+    list = (struct CFG_LIST *)malloc (sizeof (struct CFG_LIST));
+    if (list != NULL) {
+        list->next = NULL;
+        list->line = (char *)malloc (strlen(line)+1);
+        if (list->line != NULL) {
+            *(list->line) = '\0';
+            (void) snprintf (list->line, strlen(line)+1, "%s", line);
+        } else {
+            free (list);
+            list = NULL;
         }
     }
-
-    if (mode == LIST_DELETE) {
-        list_prev->next = list->next;
-        free(list->line);
-        free(list);
-        list = NULL; }
-
     return list;
 }
 
 
 
-/* FindCongigString:
- *  Pointe sur la string selon la section et la clef.
+/* skpspc:
+ *  Passe les espaces préalables d'une string.
  */
-static char *FindConfigValuePtr(char *section, char *key, int mode)
+static char *skpspc(char *p)
+{
+    while ((*p <= 0x20) && (*p != '\0'))
+        p++;
+    return p;
+}
+
+
+/* KeyPtr:
+ *  Recherche le pointeur sur la clef de la section.
+ */
+static struct CFG_LIST *KeyPtr (char *section, char *key, struct CFG_LIST **pList)
+{
+    struct CFG_LIST *list = list_start;
+
+    while ((list != NULL)
+        && (list->line != NULL)
+        && ((*list->line != '[')
+         || (strlen(list->line) < strlen(section)+2)
+         || (list->line[strlen(section)+1] != ']')
+         || (memcmp(section, list->line+1, strlen(section)) != 0)))
+        list = list->next;
+    if (list != NULL) {
+        *pList = list;
+        list = list->next;
+    }
+    while ((list != NULL)
+        && (list->line != NULL)
+        && (*list->line != '[')
+        && (memcmp (key, list->line, strlen(key)) != 0)) {
+        if (isgraph ((int)*list->line) != 0)
+            *pList = list;
+        list = list->next;
+    }
+    return list;
+}
+
+
+
+/* ValuePtr:
+ * Retourne une valeur selon la section et la clef
+ */
+static char *ValuePtr(char *section, char *key, char *init_value)
 {
     static char *value_ptr = NULL;
-    struct CFG_LIST *list = FindConfigList(section, key, mode);
-
-    if (list == NULL)
-        return NULL;
-
-    /* Positionne sur la valeur */
-    value_ptr = SkipSpaces(list->line + strlen(key));
-    if (*(value_ptr++) != '=')
-        return NULL;
-
-    value_ptr = SkipSpaces(value_ptr);
-    if (*value_ptr < 0x20)
-        return NULL;
-
-    return value_ptr;
-}
-
-
-
-/* GetConfigString:
- * Retourne une valeur CHAR selon la section et la clef
- */
-static char *GetConfigString(char *section, char *key, char *init_value)
-{
-    char *value_ptr = FindConfigValuePtr(section, key, LIST_READ);
-
-    return ((value_ptr == NULL) || (value_ptr[0] == '\0')) ? init_value : value_ptr;
-}
-
-
-
-/* SetConfigString:
- * Enregistre une valeur CHAR selon la section et la clef
- */
-static void SetConfigString(char *section, char *key, const char *value)
-{
-    size_t new_size = strlen(key)+3+strlen(value)+1;
+    static char *p = NULL;
+    struct CFG_LIST *pList = NULL;
     struct CFG_LIST *list = NULL;
 
-    if ((value == NULL) || (value[0] == '\0'))
-        (void)FindConfigList(section, key, LIST_DELETE);
-    else
-    {
-        list = FindConfigList(section, key, LIST_WRITE);
+    value_ptr = NULL;
+    list = KeyPtr (section, key, &pList);
+    if ((list != NULL) && (list->line != NULL)) {
+        p = skpspc (list->line + strlen(key));
+        if (*p == '=')
+            value_ptr = skpspc (p+1);
+    }
+    return ((value_ptr == NULL) || (*value_ptr == '\0')) ? init_value : value_ptr;
+}
 
-        if (list == NULL) return;
 
-        free(list->line);
-        list->line = malloc(new_size);
-        (void)snprintf(list->line, new_size, "%s = %s", key, value);
+
+/* SetValue:
+ * Enregistre une valeur selon la section et la clef
+ */
+static void SetValue(char *section, char *key, const char *value)
+{
+    struct CFG_LIST *list = NULL;
+    struct CFG_LIST *pList = NULL;
+    char line[300+1] = "";
+
+    if (*value != '\0') {
+        (void)snprintf (line, 300, "%s = %s", key, value);
+        list = KeyPtr (section, key, &pList);
+        if ((list != NULL) && (list->line != NULL)) {
+            list->line = realloc (list->line, strlen (line)+1);
+            if (list->line != NULL) {
+                *(list->line) = '\0';
+                (void) snprintf (list->line, strlen(line)+1, "%s", line);
+            }
+        } else {
+            list = CreateConfigLine (line);
+            if (pList != NULL) {
+                pList->next = list;
+            }
+        }
     }
 }
 
+
+/* DeleteKey:
+ * Efface une clef
+ */
+static void DeleteKey(char *section, char *key)
+{
+    struct CFG_LIST *list = NULL;
+    struct CFG_LIST *pList = NULL;
+
+    list = KeyPtr (section, key, &pList);
+    if ((pList != NULL) && (list != NULL)) {
+        pList->next = list->next;
+        free (list);
+    }
+}
 
 
 /* LoadCfg:
@@ -250,9 +228,11 @@ static void LoadCfg(char *filename)
     char fname[MAX_PATH+1] = "";
 
     (void)snprintf (fname, MAX_PATH, "%s/.teo/%s", getenv("HOME"), filename);
-    if (access (fname, F_OK) < 0)
-        (void)snprintf (fname, MAX_PATH, "/usr/share/teo/%s", filename);
     file=fopen(fname, "r");
+    if (file == NULL) {
+        (void)snprintf (fname, MAX_PATH, "/usr/share/teo/%s", filename);
+        file=fopen(fname, "r");
+    }
 #else
     file=fopen(filename, "r");
 #endif
@@ -264,9 +244,17 @@ static void LoadCfg(char *filename)
             *(ptr--) = '\0';
 
         /* Enregistre la chaîne */
-        if ((list = InsertConfigLine(list, line)) == NULL) {
+        if (list == NULL) {
+            list_start = CreateConfigLine(line);
+            list = list_start;
+        } else {
+            list->next = CreateConfigLine(line);
+            list = list->next;
+        }
+        if (list == NULL) {
             fclose(file);
-            return; }
+            return;
+        }
     }
     fclose(file);
 }
@@ -301,42 +289,61 @@ static void SaveCfg(char *filename)
  */
 void LoadState(char *filename)
 {
+    GtkWidget *dialog;
     const char *str_p;
 
     LoadCfg(filename);
 
-    str_p = GetConfigString("teo", "speed", "exact");
+    str_p = ValuePtr("teo", "speed", "exact");
     if (strcmp(str_p, "fast") == 0)
         teo.exact_speed = FALSE;
 
-    str_p = GetConfigString("teo", "memo7", NULL);
+    str_p = ValuePtr("teo", "memo7", NULL);
     if (str_p)
-        to8_LoadMemo7(str_p);
+        load_error += (to8_LoadMemo7(str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "k7", NULL);
+    str_p = ValuePtr("teo", "k7", NULL);
     if (str_p)
-        to8_LoadK7(str_p);
+        load_error += (to8_LoadK7(str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "disk_0", NULL);
+    str_p = ValuePtr("teo", "disk_0", NULL);
     if (str_p)
-        to8_LoadDisk(0, str_p);
+        load_error += (to8_LoadDisk(0, str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "disk_1", NULL);
+    str_p = ValuePtr("teo", "disk_1", NULL);
     if (str_p)
-        to8_LoadDisk(1, str_p);
+        load_error += (to8_LoadDisk(1, str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "disk_2", NULL);
+    str_p = ValuePtr("teo", "disk_2", NULL);
     if (str_p)
-        to8_LoadDisk(2, str_p);
+        load_error += (to8_LoadDisk(2, str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "disk_3", NULL);
+    str_p = ValuePtr("teo", "disk_3", NULL);
     if (str_p)
-        to8_LoadDisk(3, str_p);
+        load_error += (to8_LoadDisk(3, str_p) == TO8_ERROR) ? 1 : 0;
 
-    str_p = GetConfigString("teo", "autosave", "autosave.img");
-    if (str_p)
+    str_p = ValuePtr("teo", "autosave", "autosave.img");
+    if ((str_p) && (load_error == 0))
         to8_LoadImage(str_p);
-    
+
+    str_p = ValuePtr("teo", "load_error", "no");
+    if (strcmp(str_p, "yes") == 0)
+        to8_Reset();
+
+    if (load_error != 0) {
+        dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,
+                 is_fr?"Un fichier de configuration n'a pas pu Ãªtre " \
+                       "chargÃ©. VÃ©rifiez qu'il n'a pas Ã©tÃ© dÃ©placÃ©, " \
+                       "dÃ©truit et que le pÃ©riphÃ©rique a bien Ã©tÃ© montÃ©."
+                      :"A configuration file was unable to be loaded. " \
+                       "Check if this file has been moved, deleted and that " \
+                       "the device has been successfully mounted."
+                 );
+        gtk_window_set_title (GTK_WINDOW(dialog), is_fr?"Teo - Message":"Teo - Warning");
+        gtk_dialog_run (GTK_DIALOG (dialog));
+        gtk_widget_destroy (dialog);
+    }
+
     UnloadCfg();
 }
 
@@ -349,21 +356,25 @@ void SaveState(char *filename)
 {
     LoadCfg(filename);
 
-    SetConfigString("teo", "speed", teo.exact_speed ? "exact" : "fast");
+    SetValue("teo", "speed", teo.exact_speed ? "exact" : "fast");
 
-    SetConfigString("teo", "memo7", to8_GetMemo7Filename());
+    SetValue("teo", "memo7", to8_GetMemo7Filename());
 
-    SetConfigString("teo", "k7", to8_GetK7Filename());
+    SetValue("teo", "k7", to8_GetK7Filename());
 
-    SetConfigString("teo", "disk_0", to8_GetDiskFilename(0));
+    SetValue("teo", "disk_0", to8_GetDiskFilename(0));
 
-    SetConfigString("teo", "disk_1", to8_GetDiskFilename(1));
+    SetValue("teo", "disk_1", to8_GetDiskFilename(1));
 
-    SetConfigString("teo", "disk_2", to8_GetDiskFilename(2));
+    SetValue("teo", "disk_2", to8_GetDiskFilename(2));
 
-    SetConfigString("teo", "disk_3", to8_GetDiskFilename(3));
+    SetValue("teo", "disk_3", to8_GetDiskFilename(3));
 
-    to8_SaveImage(GetConfigString("teo", "autosave", "autosave.img"));
+    to8_SaveImage(ValuePtr("teo", "autosave", "autosave.img"));
+
+    DeleteKey ("teo", "load_error");
+    if (load_error != 0)
+        SetValue("teo", "load_error", "yes");
 
     SaveCfg(filename);
 
