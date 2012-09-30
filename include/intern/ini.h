3ed0be60fef3aaa0bbc162ccb6f7343413aef2c3
Fran?ois Mouret <fjjm@orange.fr>
2012-09-30 10:40:48 +0200

    Renamed from state.h

---
+++ include/intern/ini.h
@@ -0,0 +1,52 @@
+/*
+ *    TTTTTTTTTTTTTT  EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *    TTTTTTTTTTTTTT  EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EEEEEEEEEE      OO          OO
+ *          TT        EEEEEEEEEE      OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *          TT        EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *
+ *                  L'émulateur Thomson TO8
+ *
+ *  Copyright (C) 1997-2012 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
+ *                          Jérémie Guillaume
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *  Module     : intern/ini.h
+ *  Version    : 1.8.2
+ *  Créé par   : François Mouret 09/05/2012
+ *  Modifié par: François Mouret 29/09/2012
+ *
+ *  Chargement/Sauvegarde du fichier de configuration.
+ */
+
+
+#ifndef INI_H
+#define INI_H
+
+extern int   ini_Load (void);
+extern void  ini_Save (void);
+
+#endif
+
