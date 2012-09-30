704676feb5cb102da1314925d0c8b6c5216e300a
Fran?ois Mouret <fjjm@orange.fr>
2012-09-30 10:26:06 +0200

    Rename file from k7.c/Change 'gui->' into 'teo.'/Use utility functions

---
+++ src/media/cass.c
@@ -0,0 +1,291 @@
+/*
+ *    TTTTTTTTTTTTTT  EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *    TTTTTTTTTTTTTT  EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EEEEEEEEEE      OO          OO
+ *          TT        EEEEEEEEEE      OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EE              OO          OO
+ *          TT        EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *          TT        EEEEEEEEEEEEEE  OOOOOOOOOOOOOO
+ *
+ *                  L'émulateur Thomson TO8
+ *
+ *  Copyright (C) 1997-2012 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
+ *                          Jérémie Guillaume, François Mouret,
+ *                          Samuel Devulder
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *  Module     : cass.c
+ *  Version    : 1.8.2
+ *  Créé par   : Eric Botcazou avril 1999
+ *  Modifié par: Eric Botcazou 28/10/2003
+ *               Samuel Devulder 05/02/2012
+ *               François Mouret 25/04/2012 29/09/2012
+ *
+ *  Gestion des cassettes.
+ */
+
+
+#ifndef SCAN_DEPEND
+   #include <stdio.h>
+   #include <string.h>
+#endif
+
+#include "intern/errors.h"
+#include "intern/hardware.h"
+#include "intern/std.h"
+#include "intern/cass.h"  /* MacOS */
+#include "to8.h"
+
+
+#define COUNTER_RATIO   100
+
+static FILE *cass;
+static int cass_mode;
+static int cass_counter;
+static enum {
+   READ,
+   WRITE
+} current_op;
+
+
+
+/* DoLoadCass:
+ *  Ouvre le fichier et retourne le mode d'ouverture.
+ */
+static int DoLoadCass(const char filename[], int mode)
+{
+   FILE *new_cass;
+
+   if (mode == TO8_READ_WRITE) {
+      if ((new_cass=fopen(filename, "rb+")) != NULL)
+	 goto Success;
+      else
+	 mode = TO8_READ_ONLY;
+   }
+
+   if ((new_cass=fopen(filename, "rb")) != NULL)
+      goto Success;
+
+   return ErrorMessage(TO8_CANNOT_OPEN_FILE, NULL);
+
+ Success:
+   if (cass)
+      fclose(cass);
+
+   cass = new_cass;
+   cass_counter = -1;  /* position du fichier modifiée */
+
+   return mode;
+}
+
+
+
+/* DoCassStuff:
+ *  Emule le contrôleur du lecteur de cassettes.
+ */
+void DoCassStuff(int *br, int *cc)
+{
+   switch (LOAD_BYTE(0x6029)&0x1F) {
+
+      case 1:
+         STORE_BYTE(0x602A, 1);
+         *cc&=0xfe;
+	 break;
+
+      case 2:
+	 if ((cass) && !feof(cass)) {
+	    if (current_op == WRITE) {
+	       fflush(cass); /* pour se conformer à l'ANSI C */
+	       current_op=READ;
+	    }
+
+	    *br=fgetc(cass)&0xFF;
+	    *cc&=0xfe;
+
+	    cass_counter = -1;  /* position du fichier modifiée */
+	 }
+	 else {
+	    STORE_BYTE(0x602A, 0x80);
+	    *cc|=1;
+	 }
+	 break;
+
+      case 4:
+	 if (cass && (cass_mode == TO8_READ_WRITE)) {
+	    if (LOAD_BYTE(0x602A) != 4) {
+	       int i;
+
+	       if (current_op==READ) {
+		  fflush(cass); /* pour se conformer à l'ANSI C */
+		  current_op=WRITE;
+	       }
+
+	       for (i=0;i<10;i++)
+		  fputc(*br, cass);
+
+	       STORE_BYTE(0x602A, 4);
+
+	       cass_counter = -1;  /* position du fichier modifiée */
+	    }
+
+	    *cc&=0xfe;
+	 }
+	 else {
+	    STORE_BYTE(0x602A, 0x80);
+	    *cc|=1;
+	 }
+	 break;
+
+      case 8:
+	 if ((cass) && (cass_mode == TO8_READ_WRITE)) {
+	    if (current_op == READ) {
+	       fflush(cass); /* pour se conformer à l'ANSI C */
+	       current_op = WRITE;
+	    }
+
+	    fputc(*br,cass);
+	    *cc&=0xfe;
+
+	    cass_counter = -1;  /* position du fichier modifiée */
+	 }
+	 else {
+	    STORE_BYTE(0x602A, 0x80);
+	    *cc|=1;
+	 }
+	 break;
+
+      default:
+	 STORE_BYTE(0x602A, 0x10);
+	 *cc&=0xfe;
+	 break;
+   }
+}
+
+
+
+/* InitCass:
+ *  Initialise le module Cass.
+ */
+void InitCass(void)
+{
+    /* Appel routine de gestion Cass. */
+    mem.mon.bank[0][0x1A59] = TO8_TRAP_CODE;
+    mem.mon.bank[0][0x1A5A] = 0x39;
+
+    cass = NULL;
+    cass_mode = TO8_READ_ONLY;
+    cass_counter = -1;
+    current_op = READ;
+}
+
+
+
+/**********************************/
+/* partie publique                */
+/**********************************/
+
+
+/* EjectCass:
+ *  Ejecte la cassette.
+ */
+void to8_EjectCass(void)
+{
+    cass = std_fclose(cass);
+    teo.cass.file = std_free (teo.cass.file);
+    cass_counter = -1;
+    current_op = READ;
+}
+
+
+/* LoadCass:
+ *  Charge une cassette dans le lecteur et retourne le mode d'ouverture.
+ *  Retourne TO8_ERROR en cas d'échec et préserve la cassette précédemment
+ *  chargée.
+ */
+int to8_LoadCass(const char filename[])
+{
+   int ret = DoLoadCass(filename, cass_mode);
+
+   if (ret != TO8_ERROR) {
+      teo.cass.file = std_free (teo.cass.file);
+      teo.cass.file = std_strdup_printf ("%s", filename);
+      cass_mode = ret;
+   }
+
+   return ret;
+}
+
+
+
+/* SetCassMode:
+ *  Fixe le mode d'accès à la cassette. Retourne le mode en cas de succès
+ *  ou TO8_ERROR en case d'échec.
+ */
+int to8_SetCassMode(int mode)
+{
+   if (cass_mode == mode)
+      return cass_mode;
+
+   if (cass) {
+      int ret = DoLoadCass(teo.cass.file, mode);
+
+      if (ret != TO8_ERROR)
+	 cass_mode = ret;
+
+      return ret;
+   }
+   else {
+      cass_mode = mode;
+      return cass_mode;
+   }
+}
+
+
+
+/* GetCassCounter:
+ *  Retourne la valeur du compteur du lecteur.
+ */
+int to8_GetCassCounter(void)
+{
+   cass_counter = (cass ? ftell(cass)/COUNTER_RATIO : 0);
+   return cass_counter;
+}
+
+
+
+/* SetCassCounter:
+ *  Fixe la valeur du compteur du lecteur.
+ */
+void to8_SetCassCounter(int counter)
+{
+   if (cass) {
+      /* Vérifie que le compteur a réellement changé avant de repositionner
+	 le fichier, de façon à éviter les écarts de position dûs à l'arrondi
+	 lors de la division par COUNTER_RATIO.  */
+      if (counter != cass_counter) {
+	 fseek(cass, counter*COUNTER_RATIO, SEEK_SET);
+	 cass_counter = counter;
+      }
+   }
+}
+
