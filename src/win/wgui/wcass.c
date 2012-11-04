9fb962dfb552a9be734f0f1521405115ee44d7d6
Fran?ois Mouret <fjjm@orange.fr>
2012-11-04 10:43:01 +0100

    Change public fontions labels/gui into teo structure

--- src/win/wgui/wcass.c
+++ src/win/wgui/wcass.c
@@ -38,6 +38,7 @@
  *  Créé par   : Eric Botcazou 28/11/2000
  *  Modifié par: Eric Botcazou 28/10/2003
  *               François Mouret 17/09/2006 28/08/2011 18/03/2012
+ *                               24/10/2012
  *
  *  Gestion des cassettes.
  */
@@ -55,7 +56,9 @@
 
 #include "win/dialog.rh"
 #include "win/gui.h"
-#include "intern/gui.h"
+#include "intern/std.h"
+#include "intern/errors.h"
+#include "intern/cass.h"
 #include "to8.h"
 
 #define COUNTER_MAX  999
@@ -71,7 +74,8 @@ static struct STRING_LIST *path_list = NULL;
  */
 static void update_params (HWND hWnd)
 {
-    gui->cass.write_protect = (IsDlgButtonChecked(hWnd, K7_PROT_CHECK) == BST_CHECKED) ? TRUE : FALSE;
+    teo.cass.write_protect = (IsDlgButtonChecked(hWnd, K7_PROT_CHECK)
+                                 == BST_CHECKED) ? TRUE : FALSE;
     combo_index = SendDlgItemMessage(hWnd, K7_COMBO, CB_GETCURSEL, 0, 0);
 }
 
@@ -82,7 +86,7 @@ static void update_params (HWND hWnd)
  */
 static void set_counter_cass (HWND hWnd)
 {
-   int counter = to8_GetK7Counter();
+   int counter = cass_GetCounter();
 
    SetDlgItemInt(hWnd, K7_COUNTER_EDIT, counter, FALSE);
    SendDlgItemMessage(hWnd, K7_UPDOWN, UDM_SETPOS, 0, MAKELONG(counter, 0));
@@ -95,7 +99,7 @@ static void set_counter_cass (HWND hWnd)
  */
 static void rewind_cass (HWND hWnd)
 {
-   to8_SetK7Counter(0);
+   cass_SetCounter(0);
    set_counter_cass (hWnd);
 }
 
@@ -107,7 +111,7 @@ static void rewind_cass (HWND hWnd)
 static void eject_cass (HWND hWnd)
 {
     rewind_cass (hWnd);
-    to8_EjectK7();
+    cass_Eject();
 }
 
 
@@ -117,12 +121,13 @@ static void eject_cass (HWND hWnd)
  */
 static int load_cass (HWND hWnd, char *filename)
 {
-    int ret = to8_LoadK7 (filename);
+    int ret = cass_Load (filename);
 
     switch (ret)
     {
-        case TO8_ERROR :
-            MessageBox(hWnd, to8_error_msg, PROGNAME_STR, MB_OK | MB_ICONINFORMATION);
+        case ERR_ERROR :
+            MessageBox(hWnd, to8_error_msg, PROGNAME_STR,
+                               MB_OK | MB_ICONINFORMATION);
             break;
 
         case TO8_READ_ONLY :
@@ -145,10 +150,10 @@ static void toggle_check_cass (HWND hWnd)
 {
     if (IsDlgButtonChecked(hWnd, K7_PROT_CHECK) == BST_CHECKED)
     {
-        to8_SetK7Mode(TO8_READ_ONLY);
+        cass_SetMode(TO8_READ_ONLY);
     }
     else
-    if (to8_SetK7Mode(TO8_READ_WRITE)==TO8_READ_ONLY)
+    if (cass_SetMode(TO8_READ_WRITE)==TO8_READ_ONLY)
     {
         MessageBox(hWnd, is_fr?"Ecriture impossible sur ce support."
                               :"Warning: writing unavailable on this device."
@@ -166,12 +171,13 @@ static void toggle_check_cass (HWND hWnd)
  */
 static void add_combo_entry (HWND hWnd, const char *path)
 {
-    int index = gui_StringListIndex (path_list, (char *)path);
+    int index = std_StringListIndex (path_list, (char *)path);
 
     if (index<0)
     {
-        path_list = gui_StringListAppend (path_list, (char *)path);
-        SendDlgItemMessage(hWnd, K7_COMBO, CB_ADDSTRING, 0, (LPARAM) gui_BaseName((char *)path));
+        path_list = std_StringListAppend (path_list, (char *)path);
+        SendDlgItemMessage(hWnd, K7_COMBO, CB_ADDSTRING, 0,
+                           (LPARAM) std_BaseName((char *)path));
         SendDlgItemMessage(hWnd, K7_COMBO, CB_SETCURSEL, entry_max, 0);
         entry_max++;
     }
@@ -180,7 +186,7 @@ static void add_combo_entry (HWND hWnd, const char *path)
         SendDlgItemMessage(hWnd, K7_COMBO, CB_SETCURSEL, index, 0);
         if (index != combo_index)
         {
-            (void)load_cass (hWnd, gui_StringListText (path_list, index));
+            (void)load_cass (hWnd, std_StringListText (path_list, index));
         }
     }
 }
@@ -202,7 +208,7 @@ static void init_combo (HWND hWnd)
  */
 static void clear_combo (HWND hWnd)
 {
-    free_cass_list ();
+    wcass_Free ();
     eject_cass (hWnd);
     SendDlgItemMessage(hWnd, K7_COMBO, CB_RESETCONTENT, 0, 0);
     init_combo (hWnd);
@@ -225,7 +231,7 @@ static void combo_changed (HWND hWnd)
         }
         else
         {
-            (void)load_cass(hWnd, gui_StringListText (path_list, index));
+            (void)load_cass(hWnd, std_StringListText (path_list, index));
         }
         update_params(hWnd);
     }
@@ -242,8 +248,8 @@ static void open_file (HWND hWnd)
     char def_folder[] = ".\\k7";
     OPENFILENAME ofn;
 
-    memset(&ofn, 0, sizeof(ofn));
-    ofn.lStructSize = sizeof(ofn);
+    memset(&ofn, 0, sizeof(OPENFILENAME));
+    ofn.lStructSize = sizeof(OPENFILENAME);
     ofn.hwndOwner = hWnd;
     ofn.lpstrFilter = is_fr?"Fichiers K7\0*.k7\0":"K7 files\0*.k7\0";
     ofn.nFilterIndex = 1;
@@ -253,37 +259,38 @@ static void open_file (HWND hWnd)
     ofn.Flags = OFN_FILEMUSTEXIST | OFN_HIDEREADONLY | OFN_NOCHANGEDIR;
     ofn.lpstrDefExt ="k7";
 
-    if (strlen (gui->cass.file) != 0)
-        ofn.lpstrInitialDir = gui->cass.file;
+    if (teo.cass.file != NULL)
+        ofn.lpstrInitialDir = teo.cass.file;
     else
-    if (strlen (gui->default_folder) != 0)
-        ofn.lpstrInitialDir = gui->default_folder;
+    if (teo.default_folder != NULL)
+        ofn.lpstrInitialDir = teo.default_folder;
     else
     if (access(def_folder, F_OK) == 0)
         ofn.lpstrInitialDir = def_folder;
       
     if (GetOpenFileName(&ofn))
     {
-         if (load_cass (hWnd, ofn.lpstrFile) != TO8_ERROR)
+         if (load_cass (hWnd, ofn.lpstrFile) < 0)
          {
-             add_combo_entry (hWnd, gui->cass.file);
-            (void)snprintf (gui->default_folder, MAX_PATH, "%s", gui->cass.file);
-            (void)snprintf (current_file, MAX_PATH, "%s", gui->cass.file);
+             add_combo_entry (hWnd, teo.cass.file);
+             teo.default_folder = std_free (teo.default_folder);
+             teo.default_folder = std_strdup_printf ("%s", teo.cass.file);
+            (void)snprintf (current_file, MAX_PATH, "%s", teo.cass.file);
              update_params(hWnd);
          }
     }
 }
 
 
-/* --------------------------- Partie publique ----------------------------- */
+/* ------------------------------------------------------------------------- */
 
 
-/* free_cass_list
+/* wcass_Free:
  *  Libère la mémoire utilisée par la liste des cassettes.
  */
-void free_cass_list (void)
+void wcass_Free (void)
 {
-    gui_StringListFree (path_list);
+    std_StringListFree (path_list);
     path_list=NULL;
     entry_max=0;
     combo_index=0;
@@ -291,10 +298,10 @@ void free_cass_list (void)
 
 
 
-/* CassetteTabProc:
- * Procédure pour l'onglet des cassettes.
+/* wcass_TabProc:
+ *  Procédure pour l'onglet des cassettes.
  */
-int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+int CALLBACK wcass_TabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
    static int first=1;
    int state;
@@ -311,8 +318,8 @@ int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
          if (first)
          {
              init_combo (hWnd);
-             if (strlen(gui->cass.file) != 0)
-                 add_combo_entry (hWnd, gui->cass.file);
+             if (teo.cass.file != NULL)
+                 add_combo_entry (hWnd, teo.cass.file);
              
              combo_index = SendDlgItemMessage(hWnd, K7_COMBO, CB_GETCURSEL, 0, 0);
              first=0;
@@ -320,11 +327,12 @@ int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
          /* initialisation du combo */
          SendDlgItemMessage(hWnd, K7_COMBO, CB_RESETCONTENT, 0, 0);
          for (slist=path_list; slist!=NULL; slist=slist->next)
-             SendDlgItemMessage(hWnd, K7_COMBO, CB_ADDSTRING, 0, (LPARAM) gui_BaseName(slist->str));
+             SendDlgItemMessage(hWnd, K7_COMBO, CB_ADDSTRING, 0,
+                                (LPARAM) std_BaseName(slist->str));
          SendDlgItemMessage(hWnd, K7_COMBO, CB_SETCURSEL, combo_index, 0);
 
          /* initialisation de la protection */
-         state = (gui->cass.write_protect == TRUE) ? BST_CHECKED : BST_UNCHECKED;
+         state = (teo.cass.write_protect == TRUE) ? BST_CHECKED : BST_UNCHECKED;
          CheckDlgButton(hWnd, K7_PROT_CHECK, state);
 
          /* initialisation du compteur de cassettes */
@@ -334,17 +342,25 @@ int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
          /* initialisation des images */
          himg=LoadImage (prog_inst, "empty_ico",IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
-         SendMessage(GetDlgItem(hWnd, K7_EJECT_BUTTON), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)himg);
-         himg=LoadImage (prog_inst, "open_ico",IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
-         SendMessage(GetDlgItem(hWnd, K7_MORE_BUTTON), BM_SETIMAGE, (WPARAM)IMAGE_ICON, (LPARAM)himg);
+         SendMessage(GetDlgItem(hWnd, K7_EJECT_BUTTON), BM_SETIMAGE,
+                                (WPARAM)IMAGE_ICON, (LPARAM)himg);
+         himg=LoadImage (prog_inst, "cass_ico",IMAGE_ICON, 0, 0, LR_DEFAULTCOLOR);
+         SendMessage(GetDlgItem(hWnd, K7_MORE_BUTTON), BM_SETIMAGE,
+                                (WPARAM)IMAGE_ICON, (LPARAM)himg);
 
          /* initialisation des textes */
-         SetWindowText(GetDlgItem(hWnd, K7_COUNTER_LTEXT), is_fr?"Compteur:":" Counter:");
-         SetWindowText(GetDlgItem(hWnd, K7_REWIND_BUTTON), is_fr?"Rembobiner":"Rewind");
+         SetWindowText(GetDlgItem(hWnd, K7_COUNTER_LTEXT),
+                         is_fr?"Compteur:":" Counter:");
+         SetWindowText(GetDlgItem(hWnd, K7_REWIND_BUTTON),
+                         is_fr?"Rembobiner":"Rewind");
 
          /* initialisation des info-bulles */
-         create_tooltip (hWnd, K7_EJECT_BUTTON, is_fr?"Vider la listedes fichiers":"Empty the file list");
-         create_tooltip (hWnd, K7_MORE_BUTTON, is_fr?"Ouvrir un fichier cassette":"Open a tape file");
+         wgui_CreateTooltip (hWnd, K7_EJECT_BUTTON,
+                             is_fr?"Vider la liste des fichiers"
+                                  :"Empty the file list");
+         wgui_CreateTooltip (hWnd, K7_MORE_BUTTON,
+                             is_fr?"Ouvrir un fichier cassette"
+                                  :"Open a tape file");
 
          return TRUE;
 
@@ -378,7 +394,7 @@ int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 if (HIWORD(wParam) == EN_CHANGE)
                 {
                    counter = GetDlgItemInt(hWnd, K7_COUNTER_EDIT, NULL, FALSE);
-                   to8_SetK7Counter(counter);
+                   cass_SetCounter(counter);
                    SendMessage(counter_updown, UDM_SETPOS, 0, MAKELONG(counter, 0));
                 }
                 break;
@@ -390,7 +406,7 @@ int CALLBACK CassetteTabProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
          if ((HWND)nmupdown->hdr.hwndFrom == counter_updown)
          {
              counter = nmupdown->iPos;
-             to8_SetK7Counter(counter);
+             cass_SetCounter(counter);
              SetDlgItemInt(hWnd, K7_COUNTER_EDIT, counter, FALSE);
          }
          return TRUE;
