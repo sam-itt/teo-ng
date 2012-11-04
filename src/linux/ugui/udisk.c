f3024e826d5f46144f0cf0c50f038927245b8b5f
Fran?ois Mouret <fjjm@orange.fr>
2012-11-04 10:53:51 +0100

    Add folder selection/Change public functions labals

--- src/linux/ugui/udisk.c
+++ src/linux/ugui/udisk.c
@@ -39,6 +39,7 @@
  *  Modifié par: Eric Botcazou 19/11/2006
  *               Gilles Fétis 27/07/2011
  *               François Mouret 07/08/2011 24/03/2012 12/06/2012
+ *                               04/11/2012
  *
  *  Gestion des disquettes.
  */
@@ -52,14 +53,17 @@
    #include <gtk/gtk.h>
 #endif
 
+#include "to8err.h"
 #include "intern/std.h"
+#include "intern/disk.h"
 #include "linux/gui.h"
 #include "to8.h"
 
 #define NDISKS 4
 
 struct FILE_VECTOR {
-    int first;
+    int first_file;
+    int first_dir;
     int id;
     int direct;
     int entry_max;
@@ -78,11 +82,11 @@ static struct FILE_VECTOR vector[NDISKS];
  */
 static int load_disk (gchar *filename, struct FILE_VECTOR *vector)
 {
-    int ret = to8_LoadDisk (vector->id, filename);
+    int ret = disk_Load (vector->id, filename);
 
     switch (ret)
     {
-        case TO8_ERROR :
+        case ERR_ERROR :
             error_box (to8_error_msg, wControl);
             break;
 
@@ -105,10 +109,10 @@ static void toggle_check_disk(GtkWidget *button, struct FILE_VECTOR *vector)
 {
     if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (button)))
     {
-        to8_SetDiskMode (vector->id, TO8_READ_ONLY);
+        disk_SetMode (vector->id, TO8_READ_ONLY);
         teo.disk[vector->id].write_protect = TRUE;
     }
-    else if (to8_SetDiskMode (vector->id, TO8_READ_WRITE)==TO8_READ_ONLY)
+    else if (disk_SetMode (vector->id, TO8_READ_WRITE)==TO8_READ_ONLY)
     {
         error_box(is_fr?"Ecriture impossible sur ce support."
                        :"Writing unavailable on this device.", wControl);
@@ -126,10 +130,10 @@ static void set_access_mode (struct FILE_VECTOR *vector)
 {
     int ret;
 
-    (void)to8_VirtualSetDrive(vector->id);
+    (void)disk_SetVirtual (vector->id);
     ret = TO8_READ_WRITE;
     if ((vector->direct) && (gtk_combo_box_get_active (GTK_COMBO_BOX(vector->combo)) == 1))
-        ret = to8_DirectSetDrive(vector->id);
+        ret = disk_SetDirect (vector->id);
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(vector->check_prot), (ret==TO8_READ_ONLY) ? TRUE : FALSE);
     teo.disk[vector->id].write_protect = (ret==TO8_READ_ONLY) ? TRUE : FALSE;
 }
@@ -195,7 +199,7 @@ static void reset_combo (GtkButton *button, struct FILE_VECTOR *vector)
 
     free_disk_entry (vector);
     gtk_combo_box_text_remove_all (GTK_COMBO_BOX_TEXT(vector->combo));
-    to8_EjectDisk(vector->id);
+    disk_Eject(vector->id);
     init_combo (vector);
 
     /* Débloque l'intervention de combo_changed */
@@ -214,7 +218,7 @@ static void combo_changed (GtkComboBox *combo_box, struct FILE_VECTOR *vector)
     guint active_row = gtk_combo_box_get_active (combo_box);
 
     if (active_row==0)
-        to8_EjectDisk(vector->id);
+        disk_Eject(vector->id);
     else
     if ((active_row!=1) || (vector->direct!=1))
         (void)load_disk ((char *)g_list_nth_data (vector->path_list, active_row), vector);
@@ -232,8 +236,9 @@ static void open_file (GtkButton *button, struct FILE_VECTOR *vector)
     GtkFileFilter *filter;
     static GtkWidget *dialog;
     char *folder_name;
+    char *file_name;
 
-    if (vector->first) {
+    if (vector->first_file) {
         dialog = gtk_file_chooser_dialog_new (
                  is_fr?"SÃ©lectionner une disquette":"Select a disk",
                  (GtkWindow *) wControl, GTK_FILE_CHOOSER_ACTION_OPEN,
@@ -258,12 +263,13 @@ static void open_file (GtkButton *button, struct FILE_VECTOR *vector)
         while (gtk_events_pending ())
             gtk_main_iteration ();
 
-        vector->first=0;
+        vector->first_file=0;
     }
 
     if (gtk_dialog_run ((GtkDialog *)dialog) == GTK_RESPONSE_ACCEPT)
     {
-        if (load_disk (gtk_file_chooser_get_filename((GtkFileChooser *)dialog), vector) != TO8_ERROR)
+        file_name = gtk_file_chooser_get_filename((GtkFileChooser *)dialog);
+        if (load_disk (file_name, vector) >= 0)
         {
             /* Bloque l'intervention de combo_changed */
             g_signal_handler_block (vector->combo, vector->combo_changed_id);
@@ -271,12 +277,69 @@ static void open_file (GtkButton *button, struct FILE_VECTOR *vector)
             add_combo_entry (teo.disk[vector->id].file, vector);
             folder_name = gtk_file_chooser_get_current_folder ((GtkFileChooser *)dialog);
             teo.default_folder = std_free(teo.default_folder);
-            teo.default_folder = std_strdup_printf ("%s", folder_name);
+            if (folder_name != NULL)
+                teo.default_folder = std_strdup_printf ("%s", folder_name);
             g_free (folder_name);
 
             /* Débloque l'intervention de combo_changed */
             g_signal_handler_unblock (vector->combo, vector->combo_changed_id); 
         }
+        g_free (file_name);
+    }
+    gtk_widget_hide(dialog);
+    (void)button;
+}
+
+
+
+/* open_dir:
+ *  Charge un nouveau répertoire (callback).
+ */
+static void open_dir (GtkButton *button, struct FILE_VECTOR *vector)
+{
+    static GtkWidget *dialog;
+    char *folder_name;
+
+    if (vector->first_dir) {
+        dialog = gtk_file_chooser_dialog_new (
+                 is_fr?"SÃ©lectionner un rÃ©pertoire":"Select a folder",
+                 (GtkWindow *) wControl, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                 GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                 GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);
+
+        if (teo.disk[vector->id].file != NULL)
+            (void)gtk_file_chooser_set_filename ((GtkFileChooser *)dialog, teo.disk[vector->id].file);
+        else
+        if (teo.default_folder != NULL)
+            (void)gtk_file_chooser_set_filename ((GtkFileChooser *)dialog, teo.default_folder);
+        else
+        if (access("./disks/", F_OK) == 0)
+            (void)gtk_file_chooser_set_filename ((GtkFileChooser *)dialog, "./disks/");
+
+        /* Attend que le dialog ait tout assimilé */
+        while (gtk_events_pending ())
+            gtk_main_iteration ();
+
+        vector->first_dir=0;
+    }
+
+    if (gtk_dialog_run ((GtkDialog *)dialog) == GTK_RESPONSE_ACCEPT)
+    {
+        folder_name = gtk_file_chooser_get_filename ((GtkFileChooser *)dialog);
+        if (load_disk (folder_name, vector) >= 0)
+        {
+            /* Bloque l'intervention de combo_changed */
+            g_signal_handler_block (vector->combo, vector->combo_changed_id);
+
+            add_combo_entry (teo.disk[vector->id].file, vector);
+            teo.default_folder = std_free(teo.default_folder);
+            if (folder_name != NULL)
+                teo.default_folder = std_strdup_printf ("%s",folder_name);
+
+            /* Débloque l'intervention de combo_changed */
+            g_signal_handler_unblock (vector->combo, vector->combo_changed_id); 
+        }
+        g_free (folder_name);
     }
     gtk_widget_hide(dialog);
     (void)button;
@@ -328,7 +391,8 @@ void init_disk_notebook_frame (GtkWidget *notebook)
     {
         memset (&vector[i], 0x00, sizeof (struct FILE_VECTOR));
         vector[i].id=i;
-        vector[i].first=1;
+        vector[i].first_file=1;
+        vector[i].first_dir=1;
 
         /* boîte horizontale */
         hbox=gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
@@ -367,11 +431,19 @@ void init_disk_notebook_frame (GtkWidget *notebook)
                                                        G_CALLBACK(combo_changed), (gpointer)&vector[i]);
         /* bouton d'ouverture de fichier */
         widget = gtk_button_new ();
-        image = gtk_image_new_from_stock ("gtk-open", GTK_ICON_SIZE_BUTTON);
+        image = gtk_image_new_from_stock ("gtk-floppy", GTK_ICON_SIZE_BUTTON);
         gtk_button_set_image(GTK_BUTTON(widget), image);
         gtk_widget_set_tooltip_text (widget, is_fr?"Ouvrir un fichier disquette":"Open a disk file");
         gtk_box_pack_start( GTK_BOX(hbox), widget, FALSE, FALSE, 0);
         (void)g_signal_connect(G_OBJECT(widget), "clicked", G_CALLBACK(open_file), (gpointer)&vector[i]);
+        
+        /* bouton d'ouverture de fichier */
+        widget = gtk_button_new ();
+        image = gtk_image_new_from_stock ("gtk-directory", GTK_ICON_SIZE_BUTTON);
+        gtk_button_set_image(GTK_BUTTON(widget), image);
+        gtk_widget_set_tooltip_text (widget, is_fr?"Ouvrir un rÃ©pertoire":"Open a folder");
+        gtk_box_pack_start( GTK_BOX(hbox), widget, FALSE, FALSE, 0);
+        (void)g_signal_connect(G_OBJECT(widget), "clicked", G_CALLBACK(open_dir), (gpointer)&vector[i]);
     }
 }
 
