69b178e10f3a8e481464a9082e3beb5fe74bf7c8
Fran?ois Mouret <fjjm@orange.fr>
2013-04-18 19:08:50 +0200

    Adapt disk management to disk controller

--- include/media/disk.h
+++ include/media/disk.h
@@ -14,7 +14,7 @@
  *
  *                  L'émulateur Thomson TO8
  *
- *  Copyright (C) 1997-2012 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
+ *  Copyright (C) 1997-2013 Gilles Fétis, Eric Botcazou, Alexandre Pukall,
  *                          Jérémie Guillaume, François Mouret
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -33,63 +33,85 @@
  */
 
 /*
- *  Module     : intern/disk.h
+ *  Module     : media/disk.h
  *  Version    : 1.8.2
  *  Créé par   : Alexandre Pukall mai 1998
  *  Modifié par: Eric Botcazou 24/10/2003
  *               François Mouret 05/10/2012
- *  Gestion du format SAP 2.0: lecture et écriture disquette.
+ *
+ *  Gestion des disquettes.
  */
 
 
-#ifndef DISK_H
-#define DISK_H
+#ifndef MEDIA_DISK_H
+#define MEDIA_DISK_H
 
-#include "defs.h"
+#define TEO_DISK_SECTOR_SIZE_MAX  256
+#define TEO_RAMDISK_SECTOR_SIZE   256
+#define TEO_DISK_TRACK_NUMBER_MAX  80
 
+// #define SECTOR_PER_TRACK         16
 
-extern struct DISK_CTRL disk_ctrl;
+#define TEO_DISK_SECTOR_COUNT    16
 
-extern void disk_ctrl_cmd0(int);
+struct DISK_VECTOR {
+    char   *str;
+    int    side;
+    int    side_count;
+    struct DISK_VECTOR *next;
+};
 
-#define DISK_CTRL_SET_FUNC(name)    \
-    void disk_ctrl_##name(int val)  \
-    {                               \
-        disk_ctrl.name = val;       \
-    }
+enum {
+    TEO_DISK_ACCESS_NONE = 1,
+    TEO_DISK_ACCESS_DIRECT,
+    TEO_DISK_ACCESS_SAP,
+    TEO_DISK_ACCESS_HFE,
+    TEO_DISK_ACCESS_FD
+};
 
-extern inline DISK_CTRL_SET_FUNC(cmd1)
-extern inline DISK_CTRL_SET_FUNC(cmd2)
-extern inline DISK_CTRL_SET_FUNC(wdata)
-extern inline DISK_CTRL_SET_FUNC(wclk)
-extern inline DISK_CTRL_SET_FUNC(wsect)
-extern inline DISK_CTRL_SET_FUNC(wtrck)
-extern inline DISK_CTRL_SET_FUNC(wcell)
+typedef struct {
+    int   state;
+    int   side_count;
+    char  *tmp;
+    int   format;
+    int   (*WriteCtrlTrack) (const char filename [], struct DISK_INFO *info);
+    int   (*ReadCtrlTrack) (const char filename [], struct DISK_INFO *info);
+    int   (*ReadCtrlSector) (const char filename [], struct DISK_INFO *info);
+    int   (*WriteCtrlSector) (const char filename [], int buffer, struct DISK_INFO *info);
+    int   (*FormatCtrlTrack) (const char filename [], struct DISK_INFO *info);
+    struct DISK_INFO *info;
+} DISK_PARAMETER;
 
-extern int disk_ctrl_stat0(void);
-extern int disk_ctrl_stat1(void);
+extern DISK_PARAMETER disk[NBDRIVE];
 
-extern inline int disk_ctrl_rdata(void)
-{
-    return disk_ctrl.rdata;
-}
-
-extern void ResetDiskCtrl(int *);
-extern void ReadSector(int *);
-extern void WriteSector(int *);
-extern void FormatDrive(int *);
-extern void DiskNop(int *);
-
-extern void  disk_Init(void);
-extern int   disk_SetDirect(int drive);
-extern int   disk_SetVirtual(int drive);
-extern void  disk_UnloadDir (int drive);
-extern void  disk_Eject(int drive);
+extern int   disk_Init (void);
 extern void  disk_UnloadAll (void);
+extern void  disk_FirstLoad (void);
+extern int   disk_IsDisk (const char filename[]);
+extern int   disk_SetProtection(int drive, int mode);
+extern void  disk_Eject(int drive);
+
+extern int   disk_IsSDFloppySector (int sector, struct DISK_INFO *info);
+extern int   disk_IsDDFloppySector (int sector, struct DISK_INFO *info);
+extern void  disk_CreateDDFloppySector (int track, int sector,
+                           uint8 *sector_buffer, uint8 *data, uint8 *clock);
+extern void  disk_CreateSDFloppySector (int track, int sector,
+                           uint8 *sector_buffer, uint8 *data, uint8 *clock);
+extern int   disk_AllocRawTracks (int track_size, struct DISK_INFO *info);
+extern int   disk_ComputeCrc (uint8 *buffer, int length, int start_value);
+extern int   disk_CheckFile (const char filename[], int mode);
 extern int   disk_Check (const char filename[]);
 extern int   disk_Load(int drive, const char filename[]);
-extern void  disk_FirstLoad (void);
-extern int   disk_SetMode(int drive, int mode);
+extern int   disk_ReadCtrlTrack (int drive);
+extern int   disk_WriteCtrlTrack (int drive);
+extern void  disk_BuildSectorMap (int *sector_map, int factor);
+
+extern int   disk_DiskVectorIndex (struct DISK_VECTOR *p, const char str[]);
+extern int   disk_DiskVectorLength (struct DISK_VECTOR *p);
+extern char *disk_DiskVectorText (struct DISK_VECTOR *p, int index);
+extern struct DISK_VECTOR *disk_DiskVectorPtr (struct DISK_VECTOR *p, int index);
+extern struct DISK_VECTOR *disk_DiskVectorAppend (struct DISK_VECTOR *p, const char str[], int side, int side_count);
+extern void  disk_DiskVectorFree (struct DISK_VECTOR *p);
 
 #endif
 
