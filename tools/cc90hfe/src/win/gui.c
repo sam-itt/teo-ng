9c40110d870a1f99761a5ab2127b4be9b683ea8b
Fran?ois Mouret <fjjm@orange.fr>
2013-05-08 12:04:59 +0200

    Fix disabling of cancel button

--- tools/cc90hfe/src/win/gui.c
+++ tools/cc90hfe/src/win/gui.c
@@ -237,7 +237,7 @@ void gui_EnableButtons (int flag)
     Button_Enable(archive_button, flag);
     Button_Enable(extract_button, flag);
     Button_Enable(install_button, flag);
-    Button_Enable(install_button, (flag == TRUE) ? FALSE : TRUE);
+    Button_Enable(cancel_button, (flag == TRUE) ? FALSE : TRUE);
 }
 
 
