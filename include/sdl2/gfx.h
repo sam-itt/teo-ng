6dd3eefdf151082e154da85b5e004042917c6fd6
Samuel <samuel.cuella@gmail.com>
2020-02-06 05:28:16 +0100

    Move pixel get/set functions into gfx.c

--- include/sdl2/gfx.h
+++ include/sdl2/gfx.h
@@ -3,6 +3,10 @@
 
 #include <SDL.h>
 
+
+void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
+Uint32 getpixel(SDL_Surface *surface, int x, int y);
+
 int rectangleColor(SDL_Surface * dst, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Uint32 color);
 int rectangleRGBA(SDL_Surface * dst, Sint16 x1, Sint16 y1, Sint16 x2, Sint16 y2, Uint8 r, Uint8 g, Uint8 b, Uint8 a);
 
--- src/sdl2/teo-sdl-gfx.c
+++ src/sdl2/teo-sdl-gfx.c
@@ -4,6 +4,7 @@
 
 #include "teo.h"
 #include "std.h"
+#include "gfx.h"
 #include "sdl2/sfront.h"
 #include "sdl2/teo-sdl-vkbd.h"
 #include "sdl2/gui/sdlgui.h"
@@ -234,72 +235,6 @@ static void teoSDL_GfxSetBorderColor(int mode, int color)
                           putpixel(gpl_buffer, 4*(i)+1, 0, (val)); \
                           putpixel(gpl_buffer, 4*(i)+2, 0, (val)); \
                           putpixel(gpl_buffer, 4*(i)+3, 0, (val))
-
-/*Straight from SDL1.2 doc */
-static void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
-{
-    int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to set */
-    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
-
-    switch(bpp) {
-    case 1:
-        *p = pixel;
-        break;
-
-    case 2:
-        *(Uint16 *)p = pixel;
-        break;
-
-    case 3:
-        if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-            p[0] = (pixel >> 16) & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = pixel & 0xff;
-        } else {
-            p[0] = pixel & 0xff;
-            p[1] = (pixel >> 8) & 0xff;
-            p[2] = (pixel >> 16) & 0xff;
-        }
-        break;
-
-    case 4:
-        *(Uint32 *)p = pixel;
-        break;
-    }
-}
-
-/*Straight from SDL1.2 doc */
-Uint32 getpixel(SDL_Surface *surface, int x, int y)
-{
-    int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to retrieve */
-    Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
-
-    switch(bpp) {
-    case 1:
-        return *p;
-        break;
-
-    case 2:
-        return *(Uint16 *)p;
-        break;
-
-    case 3:
-        if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
-            return p[0] << 16 | p[1] << 8 | p[2];
-        else
-            return p[0] | p[1] << 8 | p[2] << 16;
-        break;
-
-    case 4:
-        return *(Uint32 *)p;
-        break;
-
-    default:
-        return 0;       /* shouldn't happen, but avoids warnings */
-    }
-}
 
 
 /* gpl_need_update:
